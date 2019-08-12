5f6fa94237ea04546227cb59c94df6c5f9599f98
Jens Klimke <jens.klimke@rwth-aachen.de>
2019-08-12 08:23:11 +0200

    Added namespace to library type header

--- include/SimMap/types.h
+++ include/SimMap/types.h
@@ -7,81 +7,84 @@
 
 #include <iostream>
 
-enum ObjectType {
-    STOP_SIGN, SPEED_LIMIT, UNKNOWN
-};
+namespace simmap {
 
-enum Direction {
-    FORWARDS, BACKWARDS
-};
+    enum ObjectType {
+        STOP_SIGN, SPEED_LIMIT, UNKNOWN
+    };
 
-enum Access {
-    ALLOWED, NOT_ALLOWED, NOT_POSSIBLE
-};
+    enum Direction {
+        FORWARDS, BACKWARDS
+    };
 
-
-struct Position {
-    double x;
-    double y;
-    double z;
-    double phi;
-    double kappa;
-};
+    enum Access {
+        ALLOWED, NOT_ALLOWED, NOT_POSSIBLE
+    };
 
 
-struct MapPosition {
-    const char* edgeID;
-    double longPos;
-    double latPos;
-};
+    struct Position {
+        double x;
+        double y;
+        double z;
+        double phi;
+        double kappa;
+    };
 
 
-struct HorizonInformation {
-    double s;
-    double x;
-    double y;
-    double psi;
-    double kappa;
-    double laneWidth;
-    double rightWidth;
-    double leftWidth;
-};
+    struct MapPosition {
+        const char *edgeID;
+        double longPos;
+        double latPos;
+    };
 
 
-struct ObjectInformation {
-    const char* id;
-    double distance;
-    enum ObjectType type;
-    int value;
-};
+    struct HorizonInformation {
+        double s;
+        double x;
+        double y;
+        double psi;
+        double kappa;
+        double laneWidth;
+        double rightWidth;
+        double leftWidth;
+    };
 
 
-struct LaneInformation {
-    int index;
-    double width;
-    enum Direction direction;
-    double lengthOnTrack;
-    double lengthToClosed;
-    enum Access access;
-    double s;
-    const char* id;
-};
+    struct ObjectInformation {
+        const char *id;
+        double distance;
+        enum ObjectType type;
+        int value;
+    };
 
 
-struct TargetInformation {
-    unsigned int id;
-    double distance;
-    double latOffset;
-    int lane;
-};
+    struct LaneInformation {
+        int index;
+        double width;
+        enum Direction direction;
+        double lengthOnTrack;
+        double lengthToClosed;
+        enum Access access;
+        double s;
+        const char *id;
+    };
 
 
-inline std::ostream &operator<< (std::ostream &os, const Position& pos) {
+    struct TargetInformation {
+        unsigned int id;
+        double distance;
+        double latOffset;
+        int lane;
+    };
+
+}
+
+inline std::ostream &operator<< (std::ostream &os, const simmap::Position& pos) {
     os << "xyz=(" << pos.x << "," << pos.y << "," << pos.z << "),psi=" << pos.phi << ",kappa=" << pos.kappa;
     return os;
 }
 
-inline std::ostream &operator<< (std::ostream &os, const MapPosition& pos) {
+inline std::ostream &operator<< (std::ostream &os, const simmap::MapPosition& pos) {
     os << "edge:" << pos.edgeID << ",s=" << pos.longPos << ",d=" << pos.latPos;
     return os;
 }
